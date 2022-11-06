803d84b93a5f5345d8c22c51aba5680b6f33c2c6
Steve <ycsteven.tsao@gmail.com>
2022-11-07 00:45:17 +0800

    refactor:class dellAnalyzer

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,73 +1,32 @@
 import fs from 'fs';
 import path from 'path';
 import superagent from 'superagent';
-import cheerio from 'cheerio';
-
-interface Course {
-  title: string;
-  count: number;
-}
-
-interface CourseInfo {
-  time: number;
-  data: Course[];
-}
-
-interface Content {
-  [propName: string]: Course[];
-}
+import DellAnalyzer from './dellAnalyzer';
 class Crawler {
-  private secret = 'secretKey';
-  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
-  private rawHTML = '';
-  private filePath = path.resolve(__dirname, '../data/course.json');
-
-  constructor() {
+  constructor(private analyzer: any) {
     this.initSpiderProcess();
   }
 
+  private filePath = path.resolve(__dirname, '../data/course.json');
+  private secret = 'secretKey';
+  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+  private rawHTML = '';
+
   async initSpiderProcess() {
     const rawHtml = await this.getRawHtml();
-    const courseInfo = this.getCourseInfo(rawHtml);
-    const fileContent = this.generateJsonContent(courseInfo);
-    this.writeFile(JSON.stringify(fileContent));
+    const fileContent = this.analyzer.analyze(rawHtml, this.filePath);
+    this.writeFile(fileContent);
   }
 
   private writeFile(fileContent: string) {
     fs.writeFileSync(this.filePath, fileContent);
   }
 
-  generateJsonContent(courseInfo: CourseInfo) {
-    let fileContent: Content = {};
-    if (fs.existsSync(this.filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
-    }
-    fileContent[courseInfo.time] = courseInfo.data;
-    return fileContent;
-  }
-
   async getRawHtml() {
     const result = await superagent.get(this.url);
     return result.text;
   }
-
-  getCourseInfo(html: string) {
-    const $ = cheerio.load(html);
-    const courseItems = $('.course-item');
-    const courseInfos: Course[] = [];
-
-    courseItems.map((index, element) => {
-      const descs = $(element).find('.course-desc');
-      const title = descs.eq(0).text();
-      const count = parseInt(descs.eq(1).text().split('：')[1], 10);
-      courseInfos.push({ title, count });
-    });
-
-    return {
-      time: new Date().getTime(),
-      data: courseInfos,
-    };
-  }
 }
 
-const crawler = new Crawler();
+const analyzer = new DellAnalyzer();
+const crawler = new Crawler(analyzer);
---
+++ src/dellAnalyzer.ts
@@ -0,0 +1,57 @@
+import fs from 'fs';
+import path from 'path';
+import cheerio from 'cheerio';
+
+interface Course {
+  title: string;
+  count: number;
+}
+
+interface CourseInfo {
+  time: number;
+  data: Course[];
+}
+
+interface Content {
+  [propName: string]: Course[];
+}
+
+export default class DellAnalyzer {
+  constructor() {}
+  private filePath = path.resolve(__dirname, '../data/course.json');
+  private getCourseInfo(html: string) {
+    const $ = cheerio.load(html);
+    const courseItems = $('.course-item');
+    const courseInfos: Course[] = [];
+
+    courseItems.map((index, element) => {
+      const descs = $(element).find('.course-desc');
+      const title = descs.eq(0).text();
+      const count = parseInt(descs.eq(1).text().split('：')[1], 10);
+      courseInfos.push({ title, count });
+    });
+
+    return {
+      time: new Date().getTime(),
+      data: courseInfos,
+    };
+  }
+
+  generateJsonContent(courseInfo: CourseInfo, filePath: string) {
+    let fileContent: Content = {};
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
+    }
+    fileContent[courseInfo.time] = courseInfo.data;
+    return fileContent;
+  }
+
+  /**
+   * analyze
+   */
+  public analyze(rawHtml: string, filePath: string) {
+    const courseInfo = this.getCourseInfo(rawHtml);
+    const fileContent = this.generateJsonContent(courseInfo, filePath);
+    return JSON.stringify(fileContent);
+  }
+}
